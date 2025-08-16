ed667b8ab5d6dd2091b89b318c9110cd8445296d
vishalP143 <vishal.p8070@gmail.com>
2025-08-16 15:28:25 +0000

    new p

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
